e13630c05b80e43c4ec906e818701a48ffce5e32
salsa-project <intenso524@gmail.com>
2020-05-15 03:50:51 +0100

    [Done][mailchimp] success msg after subscribe

--- src/components/MailChimpForm.js
+++ src/components/MailChimpForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import addToMailchimp from 'gatsby-plugin-mailchimp'
 import style from '../styles/EmailListForm.module.css';
 import logo from '../images/logo.svg'
@@ -6,6 +6,10 @@ import logo from '../images/logo.svg'
 function MailChimpForm(){
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
+  const [mailState, setMailState] = useState('نشكرك ﻹشتراك في نشرتنا البريدية');
+
+  const mailStateRef = useRef(null);
+  const mailInputsRef = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,7 +17,8 @@ function MailChimpForm(){
       addToMailchimp(email, {name: name}).then(data => {
         console.log(data)
         if(data.result = 'success'){
-
+          mailInputsRef.current.classList.toggle(`${style.inputWraperOff}`);
+          mailStateRef.current.classList.toggle(`${style.mailchimpStateOff}`);
         }
       })
     }
@@ -32,7 +37,10 @@ function MailChimpForm(){
       <img src={logo} className={style.mailchimpFormLogo} alt="logo"/>
       <h2>سجل في نشرتنا البريدية</h2>
       <p>سجل لتتوصل بجديدنا أولا بأول و أيضا نشرة بريدية خاصة لما يحصل خلف الستار من مواضيع لا أنشرها على المدونة</p>
-      <div>
+      <div className={`${style.mailchimpState}`} ref={mailStateRef}>
+        <p>نشكرك على إشتراكك بالنشرة البريدية</p>
+      </div>
+      <div className={`${style.inputWraper}`} ref={mailInputsRef}>
         <input
         placeholder="البريد الإلكتروني"
         name="email"
